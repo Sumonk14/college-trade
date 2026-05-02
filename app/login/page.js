b43aa207c8553d2f195392379365974a9b5903d9
Sumonk14 <maitreya2all@gmail.com>
2026-05-03 00:05:45 +0530

    add email password auth

--- app/login/page.js
+++ app/login/page.js
@@ -1,49 +1,47 @@
 "use client"
-import { use, useState } from "react"
+import { useState } from "react"
 import { createClient } from "../lib/supabase"
+import { useRouter } from "next/navigation"
 
 export default function Login() {
   const supabase = createClient()
   const [email, setEmail] = useState("")
-  const [sent, setSent] = useState(false)
+  const [password, setPassword] = useState("")
+  const router = useRouter()
 
   async function handleLogin(e) {
     e.preventDefault()
-    const { error } = await supabase.auth.signInWithOtp({
-      email,
-      options: {
-        emailRedirectTo:  window.location.origin
-      }
-    })
-    if (error) {
-      console.log(error)
-      return
+    const { error } = await supabase.auth.signInWithPassword({ email, password})
+    if (error) {console.log(error); return}
+    router.push("/")   
     }
-    setSent(true)   
-  }
-
-  if (sent) {
-    return (
-      <main className="max-w-md mx-auto p-8 mt-20 text-center">
-        <h1 className="text-2xl font-bold mb-4">Check your email</h1>
-        <p className="text-gray-600">We sent a magic link to {email}. Click it to log in.</p>
-      </main>
-    )
-  }
+  async function handleSignup(e) {
+    e.preventDefault()
+    const { error } = await supabase.auth.signUp({ email, password})
+    if (error) { console.log(error); return }
+    router.push("/")     
+    }
 
   return (
     <main className="max-w-md mx-auto p-8 mt-20">
       <h1 className="text-3xl font-bold mb-6">Login to sell</h1>
-      <form onSubmit={handleLogin} className="flex fles=x-col gap-4">
+      <form onSubmit={handleLogin} className="flex flex-col gap-4">
         <input 
-          placeholder="Your college email"
+          placeholder="Your email"
           value={email}
           onChange={e => setEmail(e.target.value)}
           className="border rounded p-2"
         />
-        <button type="submit" className="bg-blue-600 text-white rounded p-3">
-          Send Magic Link
-        </button>
+        
+        <input 
+          placeholder="Your password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className="border rounded p-2"
+        />
+        
+        <button type="button" onClick={handleLogin} className="bg-blue-600 text-white rounded p-3">Login</button>
+        <button type="button" onClick={handleSignup} className="bg-gray-600 text-white rounded p-3">Sign Up</button>
       </form>
     </main>
   )
